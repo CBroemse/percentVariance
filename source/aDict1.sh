538088a95e662f6a2f382930d76ba476e9094dba
Cee <35766903+CBroemse@users.noreply.github.com>
2022-11-11 21:32:41 +0100

    Add files via upload

---
+++ source/aDict1.sh
@@ -0,0 +1,122 @@
+100 102 98
+100 102 98
+110 121 99
+110 121 99
+120 144 96
+anton 100 102 98
+anton 100 120 80
+gabriel 100 120 80
+gabriela 100 120 80
+gabriela 100 120 80
+gabriela 100 120 80
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 c
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 c
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 percentOFF NOTHING
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 ghci -e main -interactive-print=FileSystem2.main fileSystem2
+gabriela 100 120 80 NOTHING
+Tom 1000 1300 700 NOTHING
+Tom 1000 1400 600 NOTHING
+Tom 1000 1400 600 NOTHING
+Tom 1000 1400 600 NOTHING
+Tom 100 140 60 NOTHING
+a 100 140 60 NOTHING
+a 100 110 90 NOTHING
+Tom 100 140 60 NOTHING
+Tom 100 105 95 NOTHING
+Tom 100 105 95 NOTHING
+Tom 100 105 95 NOTHING
+Tom 100 105 95 NOTHING
+Tom 100 105 95 NOTHING
+Tom 100 220 -20 NOTHING
+Tom 100 220 -20 NOTHING
+gabriela 100 220 -20 NOTHING
+gabriela 100 220 -20 NOTHING
+100 105 95
+100 105 95
+100 105 95
+gabriela 100 220 -20 NOTHING
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 NOTHING
+100 105 95
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 NOTHING
+gabriela 100 120 80 NOTHING
+a 100 120 80 NOTHING
+gabe 100 120 80 NOTHING
+gabe 100 102 98 NOTHING
+gabe 100 102 98 NOTHING
+gabe 100 102 98 NOTHING
+gabe 100 102 98 NOTHING
+gabe 100 102 98 NOTHING
+gabe 100 112 88 NOTHING
+gabe 100 112 88 NOTHING
+gabe 100 112 88 NOTHING
+gabe 100 112 88 NOTHING
+gabe 100 112 88 NOTHING
+Tom 100 105 95 GO
+Hannes 1000 1050 950 GO
+gabe 100 112 88 NOTHING
+Hans 1000 1050 950 SOp
+Peter 10000 10500 9500 NOTHING
+Peter 10000 10500 9500 SOp
+gabe 100 112 88 Boing goes off
+gabe 100 112 88 Boing goes off
+Node 100 112 88 node
+Node 100 112 88 node
+Node 100 112 88 NOTHING
+Node 100 112 88 node
+Node 100 112 88 NOTHING
+Node 100 112 88 node
+Node 100 112 88 NOTHING
+gabe 100 112 88 Boing goes off
+gabe 100 112 88 Boing goes off
+Node 500 550 450 
+Node 500 550 450 NOTHING
+100 105 95
+gabe 100 112 88 Boing goes off
+100 105 95
+gabe 100 120 80 Boing goes off:
+Start 100 110 90 A small program
+Start 100 110 90 A small program
+100 120 80
+Start 1 1.01000000 0.99000000 A small program
+Start 1 1.01000000 0.99000000 A small program
+Start 1 1.01000000 0.99000000 A small program
+Start 221.99988 2441.99868000 -1997.99892000 field1
+Start 221.99988 443.99976000 0 field1
+Start 221.99988 244.19986800 199.79989200 field1
+Start 221.99988 224.21987880 219.77988120 field1
+Start 221 223.21000000 218.79000000 field1
+Start 221 225.42000000 216.58000000 field1
+Start 721 728.21000000 713.79000000 field1
+Start 721 807.52000000 634.48000000 field1
+Start 721 757.05000000 684.95000000 field1
+Start 721 757.05000000 684.95000000 field1
+Start 721 1081.50000000 360.50000000 field1
+Start 721 724.60500000 717.39500000 field1
+Start 721 724.60500000 717.39500000 field1
+Start 721 724.60500000 717.39500000 NOTHING
+100 105 95
